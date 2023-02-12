4d103c46c7c6edfc9699f4908b30ab99d5e6b764
avivashishta29 <avivashishta@gmail.com>
2023-02-12 21:25:26 +0530

    contest 1

---
+++ contest1.js
@@ -0,0 +1,32 @@
+let myDiv = document.getElementById("flex-container");
+let newHtml;
+
+fetch("https://dummyjson.com/products")
+  .then((response) => response.json())
+  .then((data) => {
+    newHtml = data.products.map((product, i) => {
+      console.log("PRODUCT DETAILS", product.title, i);
+      return `
+        <div class='product-container'>
+        <div class="carousel-wrapper">
+        <div class="carousel-inner">
+            ${product.images
+              .map((image, index) => {
+                var active;
+                if (index == 0) {
+                  active = true;
+                }
+                return `
+                    <div class="carousel-item ${active ? "active" : ""}">
+                        <img src="${image}"/>
+                    </div>`;
+              })
+              .join("")}
+        </div>
+        </div>
+            <h1>${i}-${product.title}</h1>
+        </div>
+      `;
+    });
+    myDiv.innerHTML = newHtml.join("");
+  });
