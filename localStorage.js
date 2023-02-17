14163c2bec0e5126b8c756c7c458912739a4152c
avivashishta29 <avivashishta@gmail.com>
2023-02-17 23:28:49 +0530

    prototypes

---
+++ localStorage.js
@@ -0,0 +1,25 @@
+// If logged in, directly take the user to the profile page
+
+// first part - that save the state of the user logging in
+//second part - get this saved state so that you can take them to the page
+
+if (localStorage.getItem("isLoggedIn") === "true") {
+  window.location.href = "/profile";
+}
+
+// console.log(localStorage.getItem("isLoggedIn"));
+// var isLoggedIn = localStorage.getItem("isLoggedIn");
+
+// function login() {
+//   // save this data
+//   localStorage.setItem("isLoggedIn", true);
+//   return true;
+// }
+
+// function clickedButton() {
+//   isLoggedIn = login();
+
+//   if (isLoggedIn) {
+//     window.location.href = "/profile";
+//   }
+// }
