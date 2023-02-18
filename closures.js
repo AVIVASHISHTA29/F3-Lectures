ffede85fcdb19077ddc7605c8027e8466fd0b328
avivashishta29 <avivashishta@gmail.com>
2023-02-18 22:06:29 +0530

    closures

---
+++ closures.js
@@ -0,0 +1,25 @@
+console.log("Closures");
+
+function outSideFnc() {
+  var x = 5;
+
+  function mainFunction() {
+    //   var x = 5;
+    function addX() {
+      // var x = 20;
+      var y = 10;
+      console.log("Sum =", x + y);
+    }
+    addX();
+  }
+  mainFunction();
+}
+
+outSideFnc();
+
+// for (var i = 0; i < 3; i++) {
+//   function greet() {
+//     console.log(i);
+//   }
+//   setTimeout(greet, 1000);
+// }
